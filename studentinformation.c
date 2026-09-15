b18e5d8920f8fb5b69adc27a362ca3cb4f57f09e
manalwaseem07 <manalwaseem07@gmail.com>
2026-09-15 10:23:53 +0500

    Create studentinformation.c

---
+++ studentinformation.c
@@ -0,0 +1,30 @@
+Task 4 — Student Information Display
+Write a C program that stores and displays the following information about a student:
+ Student name
+ Roll number
+ Age
+ Height
+
+FAST UNI 4
+ GPA
+ Section character
+Use an appropriate data type for each piece of information. The program should display the
+information in a properly organized format.
+Example:
+========================================
+STUDENT INFORMATION
+========================================
+Name : Manal
+Roll No : 1023
+Age : 20
+Height : 5.4
+GPA : 3.75
+Section : A
+========================================
+Requirements
+ Use meaningful variable names.
+ Use appropriate data types.
+ Use scanf() for numeric/character input.
+ Use string input as demonstrated in the lab.
+ Use printf() for formatted output.
+ Use escape sequences to organize the output.
